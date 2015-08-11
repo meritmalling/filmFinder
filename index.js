e4f30029a5a7864822acac74343e9d198c9ccf94
merittrompeter <mktrompeter@gmail.com>
2015-08-11 09:20:08 -0700

    w07d01

--- index.js
+++ index.js
@@ -3,24 +3,17 @@ var app = express();
 var bodyParser = require('body-parser');
 var ejsLayouts = require('express-ejs-layouts');
 var request = require('request');
+var db = require("./models");
 
 require('express-helpers')(app);
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(ejsLayouts);
 
-// var filmsController = require('./controller/films');
-// app.use('/films', filmsController);
-
 app.use(express.static(__dirname + '/public'));
 
-app.get("/", function(req, res){
-  res.render('films/index.ejs');
-});
-
-app.get("/home", function(req, res){
-  res.render('films/home.ejs');
-});
+// app.use('/', require('./controllers/films.js'));
+// app.use('/favorites',require('./controllers/favorites.js'));
 
 var Film = function (title, year, rated, runtime, genre, director, writer, actors, plot, awards, year, rated, id, tomatoes, rating, poster){
   this.title = title;
@@ -41,18 +34,17 @@ var Film = function (title, year, rated, runtime, genre, director, writer, actor
   this.poster = poster
 }
 
-app.get("/favorites", function(req, res){
-  res.render('favorites/index.ejs');
-})
-
 app.get("/", function(req, res){
   res.render('films/index.ejs');
 })
 
+app.get("/home", function(req, res){
+  res.render('films/home.ejs');
+});
+
 app.get("/results", function(req, res){
   var search = req.query.q;
   var url = ('http://www.omdbapi.com/?s=' + search);
-
   request(url, function(error, response, data) {
     var filmInfo = JSON.parse(data);
     var searchResults = filmInfo.Search;
@@ -65,7 +57,6 @@ app.get("/results", function(req, res){
 app.get("/show/:id", function(req, res){
   var search = req.params.id
   var url = 'http://www.omdbapi.com/?i=' + search + '&tomatoes=true';
-
   request(url, function(error, response, data) {
     var filmInfo = JSON.parse(data);
     var title = filmInfo.Title;
@@ -87,5 +78,46 @@ app.get("/show/:id", function(req, res){
     res.render('films/show.ejs', { myFilm: thisFilm })
   });
 });
+
+////////// FAVORITES
+app.get("/favorites", function(req, res){
+  db.favorite.findAll().then(function(favorites) {
+  res.render('favorites/index.ejs', {myFavorites: favorites});
+  });
+});
+
+app.post("/favorites", function(req, res){
+  db.favorite.findOrCreate({where:
+    {imdbId: req.body.id, title: req.body.title, year: req.body.year, poster: req.body.poster}}).spread(function(aFavorite, created){
+      res.redirect('/favorites');
+    }).catch(function(error){
+      res.render('Sorry, favorite not found.');
+    });
+});
+
+app.post("/comments", function(req, res){
+  var lookup = req.body.id
+  console.log('lookup ' + lookup)
+  var commentText = req.body.comment
+  db.comment.create({
+    favoriteId: lookup,
+    comment: commentText
+  }).then(function(comment){
+      res.redirect('/favorites');
+    });
+});
+
+app.get("/favorites/show/:id", function(req, res){
+  var lookup = req.params.id
+  db.favorite.find({where: {imdbId: lookup}}).then(function(favorite) {
+    var thisId = favorite.id
+    db.comment.findAll({where: {favoriteId: thisId}}).then(function(comments){
+      res.render('favorites/show.ejs', {
+        myFavorite: favorite,
+        myComments: comments
+      });
+    });
+    });
+  });
 
 app.listen(3000);
